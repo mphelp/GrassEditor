f8607128f90ae88a47e91c3015bad3f5073fd782
Matthew Phelps <matthew.130pm@gmail.com>
2018-08-11 13:25:28 -0400

    added functionality to page UP/DOWN: move cursor to end of screen

--- grass.c
+++ grass.c
@@ -19,7 +19,9 @@ enum editorKey {
     ARROW_LEFT = 1000,
     ARROW_RIGHT,
     ARROW_UP,
-    ARROW_DOWN
+    ARROW_DOWN,
+    PAGE_UP,
+    PAGE_DOWN
 };
 
 /*** Data ***/
@@ -72,12 +74,23 @@ int editorReadKey(){
         if (read(STDIN_FILENO, &seq[1], 1) != 1) return '\x1b';
 
         if (seq[0] == '['){
-            switch (seq[1]){
-                // arrow keys
-                case 'A': return ARROW_UP;
-                case 'B': return ARROW_DOWN;
-                case 'C': return ARROW_RIGHT;
-                case 'D': return ARROW_LEFT;
+            if (seq[1] >= '0' && seq[1] <= '9'){
+                if (read(STDIN_FILENO, &seq[2], 1) != 1) return '\x1b';
+                if (seq[2] == '~'){
+                    switch (seq[1]){
+                        // page up/down -- <esc>[#~
+                        case '5': return PAGE_UP;
+                        case '6': return PAGE_DOWN;
+                    }
+                }
+            } else {
+                switch (seq[1]){
+                    // arrow keys -- <esc>[A/B/C/D
+                    case 'A': return ARROW_UP;
+                    case 'B': return ARROW_DOWN;
+                    case 'C': return ARROW_RIGHT;
+                    case 'D': return ARROW_LEFT;
+                }
             }
         }
         return '\x1b';
@@ -203,6 +216,12 @@ void editorProcessKeypress(){
             write(STDOUT_FILENO, "\x1b[2J", 4); // clear screen on safe exit
             exit(0);
             break;
+        case PAGE_UP:
+        case PAGE_DOWN:
+            {
+                int times = E.screenrows;
+                while (times--) editorMoveCursor(c == PAGE_UP? ARROW_UP:ARROW_DOWN);
+            }
         case ARROW_UP:
         case ARROW_LEFT:
         case ARROW_DOWN:
