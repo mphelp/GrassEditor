fa37c5324a63f870246c0a4bbf8fd7b73e5d1f60
Matthew Phelps <matthew.130pm@gmail.com>
2018-08-11 10:39:20 -0400

    Chapter 3 - clear lines one at a time finished

--- kilo.c
+++ kilo.c
@@ -108,6 +108,7 @@ void editorDrawRows(struct abuf* ab){
     for (y=0; y<E.screenrows; y++){
         abAppend(ab, "@", 1);
         
+        abAppend(ab, "\x1b[K", 3);
         if (y < E.screenrows - 1){
             abAppend(ab, "\r\n", 2);
         }
@@ -115,13 +116,16 @@ void editorDrawRows(struct abuf* ab){
 }
 void editorRefreshScreen(){
     struct abuf ab = ABUF_INIT;
-    abAppend(&ab, "\x1b[2J", 4); 
+
+    abAppend(&ab, "\x1b[?25l", 6);
     abAppend(&ab, "\x1b[H", 3);
 
     editorDrawRows(&ab);
 
     abAppend(&ab, "\x1b[H", 3);
-    write(STDOUT_FILENO, ab.b, ab.len);
+    abAppend(&ab, "\x1b[?25h", 6);
+
+    write(STDOUT_FILENO, ab.b, ab.len); // write out buffer 
     abFree(&ab);
 }
 
