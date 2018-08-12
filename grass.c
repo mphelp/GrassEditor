66383b21bf16bb36c161147565c3f97281f3c76d
Matthew Phelps <matthew.130pm@gmail.com>
2018-08-12 12:09:53 -0400

    Chapter 4 - start 'moving right at the end of line'

--- grass.c
+++ grass.c
@@ -280,13 +280,25 @@ void editorMoveCursor(int key){
 
     switch(key){
         case ARROW_LEFT:
-            if (E.cx != 0) E.cx--; break;
+            if (E.cx != 0) 
+                E.cx--; 
+            else if (E.cy > 0){
+                E.cy--;
+                E.cx = E.row[E.cy].size;
+            }
+            break;
         case ARROW_UP:
-            if (E.cy != 0) E.cy--; break;
+            if (E.cy != 0) 
+                E.cy--; 
+            break;
         case ARROW_RIGHT:
-            if (row && E.cx < row->size) E.cx++; break;
+            if (row && E.cx < row->size) 
+                E.cx++; 
+            break;
         case ARROW_DOWN:
-            if (E.cy < E.numrows) E.cy++; break; // cursor can extend below so long as EOF not reached
+            if (E.cy < E.numrows) 
+                E.cy++; 
+            break; // cursor can extend below so long as EOF not reached
     }
     // Snap cursor to end of line if needed
     row = (E.cy >= E.numrows) ? NULL : &E.row[E.cy];
