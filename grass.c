73f9ebdc57cd4f2f4b7a08d1fb732cbfccd2c5a5
Matthew Phelps <mphelps3@nd.edu>
2018-11-02 15:22:13 -0400

    Home, end, scroll down and up

--- grass.c
+++ grass.c
@@ -378,7 +378,8 @@ void editorProcessKeypress(){
 			E.cx = 0; 
 			break;
 		case END_KEY:
-			E.cx = E.screencols - 1; 
+			if (E.cy < E.numrows)
+				E.cx = E.row[E.cy].size;
 			break;
 		case DEL_KEY:
 			// do nothing currently
@@ -386,8 +387,15 @@ void editorProcessKeypress(){
 		case PAGE_UP:
 		case PAGE_DOWN:
 			{
+				if (c == PAGE_UP){
+					E.cy = E.rowoff;
+				} else if (c == PAGE_DOWN){
+					E.cy = E.rowoff + E.screenrows - 1;
+					if (E.cy > E.numrows) E.cy = E.numrows;
+				}
 				int times = E.screenrows;
-				while (times--) editorMoveCursor(c == PAGE_UP? ARROW_UP:ARROW_DOWN);
+				while (times--) 
+					editorMoveCursor(c == PAGE_UP ? ARROW_UP : ARROW_DOWN);
 			}
 			break;
 		case ARROW_UP:
